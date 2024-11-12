3a589caee23eb706daa5c21cb8398cecbe1d8f2f
Dominik Höll <dominikhoell06@gmail.com>
2024-11-12 10:05:59 +0100

    implemented "commentBlock" method"

--- src/TestCommentBlock.java
+++ src/TestCommentBlock.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.sql.SQLOutput;
 
 public class TestCommentBlock {
 
@@ -25,16 +26,37 @@ public class TestCommentBlock {
 		ohneComment= commentBlock(mitComment);
 		
 		System.out.println(ohneComment);
-		
-			
+
+		System.out.println("Test");
 	}
 
 
-	public static String commentBlock(String s){
-		String ret="";	
+	public static String commentBlock(String s) {
+		StringBuilder result = new StringBuilder();
+		boolean inComment = false;
+		int length = s.length();
 
-		
-		return ret;
+		for (int i = 0; i < length; i++) {
+			if (inComment) {
+				// Prüfen, ob das Ende des Blockkommentars erreicht wurde
+				if (i + 1 < length && s.charAt(i) == '*' && s.charAt(i + 1) == '/') {
+					inComment = false;
+					i++; // Das Ende des Kommentars überspringen
+				}
+			} else {
+				// Prüfen, ob der Beginn eines Blockkommentars erreicht wurde
+				if (i + 1 < length && s.charAt(i) == '/' && s.charAt(i + 1) == '*') {
+					inComment = true;
+					i++; // Den Anfang des Kommentars überspringen
+				} else {
+					// Kein Kommentar, Zeichen in die Ausgabe schreiben
+					result.append(s.charAt(i));
+				}
+			}
+		}
+
+		return result.toString();
 	}
+
 
 }
